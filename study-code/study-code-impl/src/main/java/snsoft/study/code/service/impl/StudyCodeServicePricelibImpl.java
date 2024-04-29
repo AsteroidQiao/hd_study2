f28f84c9e1d3770ec454d2726864a0b01179ebc3
AsteroidQiao <asteroidqiao@163.com>
2024-04-29 17:36:51 +0800

    修改继承的父类为DefaultFunctionService

--- study-code/study-code-impl/src/main/java/snsoft/study/code/service/impl/StudyCodeServicePricelibImpl.java
+++ study-code/study-code-impl/src/main/java/snsoft/study/code/service/impl/StudyCodeServicePricelibImpl.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Service;
 import snsoft.api.bas.QueryResults;
 import snsoft.api.bas.SaveParams;
 import snsoft.api.bas.SaveResults;
-import snsoft.ft.busi.service.FTBusiAccessServiceImpl;
+import snsoft.plat.bas.busi.service.impl.DefaultFunctionService;
 import snsoft.study.code.service.StudyCodePricelibParam;
 import snsoft.study.code.service.StudyCodePricelibService;
-import snsoft.study.code.vo.StudyAgency;
 import snsoft.study.code.vo.StudyPriceLib;
 
 /**
@@ -24,7 +23,7 @@ import snsoft.study.code.vo.StudyPriceLib;
  * 查看帮助：<a href="" target="_blank"></a>
  */
 @Service
-public class StudyCodeServicePricelibImpl extends FTBusiAccessServiceImpl<StudyPriceLib> implements StudyCodePricelibService {
+public class StudyCodeServicePricelibImpl extends DefaultFunctionService<StudyPriceLib> implements StudyCodePricelibService {
     @Override
     public QueryResults<StudyPriceLib> queryEntry(StudyCodePricelibParam params) {
         return super.uiQuery(params, null);
